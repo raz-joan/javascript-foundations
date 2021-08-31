287a268991cf68dc0fd589966b9fe07893ad64f6
Joan Rasmussen <raz.joan@gmail.com>
2021-08-31 09:00:00 -0600

    Add eatfuntion to Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -3,50 +3,19 @@ class Dragon {
     this.name = name;
     this.rider = rider;
     this.hungry = true;
+    this.timesEaten = 0;
   }
 
   greet() {
     return `Hi, ${this.rider}!`;
   }
 
-  // eat(var i = [0]) {
-  //   i[0] += 1;
-  //   console.log(i[0]);
-  //   if (i[0] === 3) {
-  //     this.hungry = false;
-  //   }
-  //   return i[0];
-  // }
-    // var count = 0;
-    // eat() {
-    //   count++;
-    //   if (count === 3) {
-    //     this.hungry = false;
-    //   };
-    //   return count;
-    // }
-  // eat(count) {
-  //   if (count === 3) {
-  //       this.hungry = false;
-  //     } else {
-  //       count++;
-  //       return count;
-  //     }
-  //   }
-
   eat() {
-    var count = 0;
-    function addCount() {
-      count++;
-    }
-    addCount();
-    console.log(count);
-    if (count === 3) {
+    this.timesEaten++;
+    if (this.timesEaten === 3) {
       this.hungry = false;
     }
-    return count;
-    }
-
+  }
 }
 
 module.exports = Dragon;
