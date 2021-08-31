770e78ca31a4fb7b1377a25e15ccba0974a18c7c
Joan Rasmussen <raz.joan@gmail.com>
2021-08-31 07:13:11 -0600

    Complete drink function on Vampire

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -7,10 +7,16 @@ class Vampire {
       this.pet = 'bat';
     }
     this.thirsty = true;
+    this.ouncesDrank = 0;
   }
 
   drink() {
     this.thirsty = false;
+    if (this.ouncesDrank < 50) {
+      this.ouncesDrank += 10;
+    } else {
+      return "I'm too full to drink anymore!";
+    }
   }
 }
 
