ead13b41c02d602ff9321d17a32359bacc14ba38
Joan Rasmussen <raz.joan@gmail.com>
2021-08-30 20:48:13 -0600

    Create Dragon class and add all but final function

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,52 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+
+  // eat(var i = [0]) {
+  //   i[0] += 1;
+  //   console.log(i[0]);
+  //   if (i[0] === 3) {
+  //     this.hungry = false;
+  //   }
+  //   return i[0];
+  // }
+    // var count = 0;
+    // eat() {
+    //   count++;
+    //   if (count === 3) {
+    //     this.hungry = false;
+    //   };
+    //   return count;
+    // }
+  // eat(count) {
+  //   if (count === 3) {
+  //       this.hungry = false;
+  //     } else {
+  //       count++;
+  //       return count;
+  //     }
+  //   }
+
+  eat() {
+    var count = 0;
+    function addCount() {
+      count++;
+    }
+    addCount();
+    console.log(count);
+    if (count === 3) {
+      this.hungry = false;
+    }
+    return count;
+    }
+
+}
+
+module.exports = Dragon;
