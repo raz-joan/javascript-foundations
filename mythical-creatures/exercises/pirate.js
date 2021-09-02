3fc1d4a3807faf4eaa1211e76f0319d68822dd19
Joan Rasmussen <raz.joan@gmail.com>
2021-09-02 07:24:54 -0600

    Make cursed after five robShip calls on Pirate

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -8,11 +8,19 @@ class Pirate {
     }
     this.cursed = false;
     this.booty = 0;
+    this.shipsRobbed = 0;
   }
 
   robShip() {
     this.booty += 100;
-    return 'YAARRR!';
+    this.shipsRobbed++;
+    if (this.shipsRobbed <= 5) {
+      return 'YAARRR!';
+    } else {
+      this.booty -= 100;
+      this.cursed = true;
+      return "ARG! I've been cursed!";
+    }
   }
 }
 
