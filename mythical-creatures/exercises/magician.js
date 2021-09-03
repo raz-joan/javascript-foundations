1bb0e67f5faeae03b1e5e938602627e6c08a7586
Joan Rasmussen <raz.joan@gmail.com>
2021-09-02 20:29:03 -0600

    Add performShowStopper() to Magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -15,11 +15,19 @@ class Magician {
   }
 
   performTrick() {
-    this.confidencePercentage += 10;  
+    this.confidencePercentage += 10;
     if (this.favoriteAccessory === 'top hat') {
       return 'PULL RABBIT FROM TOP HAT';
     } else {
       return 'PULL DOVE FROM SLEEVE';
+    }
+  }
+
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant) {
+      return "WOW! The magician totally just sawed that person in half!";
+    } else {
+      return "Oh no, this trick is not ready!";
     }
   }
 }
