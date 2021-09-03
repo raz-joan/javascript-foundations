7ab2e12db92871fedf1f85f63eb59b1b13fea575
Joan Rasmussen <raz.joan@gmail.com>
2021-09-02 20:19:52 -0600

    Add performTrick() to Magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -12,6 +12,14 @@ class Magician {
   performIncantation(spell) {
     return spell.toUpperCase() + "!";
   }
+
+  performTrick() {
+    if (this.favoriteAccessory === 'top hat') {
+      return 'PULL RABBIT FROM TOP HAT';
+    } else {
+      return 'PULL DOVE FROM SLEEVE';
+    }
+  }
 }
 
 module.exports = Magician;
