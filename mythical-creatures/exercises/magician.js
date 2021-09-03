bfd8e3eaf3f7bd8ca48308aa77b54623e34dea66
Joan Rasmussen <raz.joan@gmail.com>
2021-09-02 20:10:50 -0600

    Add favoriteAccessory to Magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,6 +2,11 @@ class Magician {
   constructor(magician) {
     this.name = `The Great ${magician.name}`;
     this.assistant = magician.assistant;
+    if (magician.clothing) {
+      this.favoriteAccessory = magician.clothing;
+    } else {
+      this.favoriteAccessory = 'top hat';
+    }
   }
 }
 
