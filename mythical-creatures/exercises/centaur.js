a320cdc7393f11566aedd7b5941a825374b2b7a1
Joan Rasmussen <raz.joan@gmail.com>
2021-09-07 07:06:59 -0600

    Add ability to stand and lay down and sleep to Centaur

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -4,10 +4,14 @@ class Centaur {
     this.breed = name.type;
     this.cranky = false;
     this.standing = true;
+    this.layingDown = false;
     this.moodMeter = 0;
   }
   shootBow() {
     this.moodMeter++;
+    if (this.layingDown) {
+      return 'NO!';
+    }
     if (this.moodMeter >= 3) {
       this.cranky = true;
       return 'NO!';
@@ -17,12 +21,30 @@ class Centaur {
   }
   run() {
     this.moodMeter++;
+    if (this.layingDown) {
+      return "NO!";
+    }
     if (this.moodMeter >= 3) {
       this.cranky = true;
       return 'NO!';
     } else {
       return 'Clop clop clop clop!!!';
     }
+  }
+  sleep() {
+    if (this.standing) {
+      return 'NO!';
+    } else {
+      return "ZZZZ";
+    }
+  }
+  layDown() {
+    this.standing = false;
+    this.layingDown = true;
+  }
+  standUp() {
+    this.standing = true;
+    this.layingDown = false;
   }
 }
 
