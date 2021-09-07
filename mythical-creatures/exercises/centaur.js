7dbe5075dc912ed8f1f1746214860680b4c1b951
Joan Rasmussen <raz.joan@gmail.com>
2021-09-07 06:44:18 -0600

    Add cranky and standing keys to Centaur

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -2,9 +2,14 @@ class Centaur {
   constructor (name) {
     this.name = name.name;
     this.breed = name.type;
+    this.cranky = false;
+    this.standing = true;
   }
   shootBow() {
-    return "Twang!!!"
+    return "Twang!!!";
+  }
+  run() {
+    return 'Clop clop clop clop!!!';
   }
 }
 
