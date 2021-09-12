47fb4341d3faddb77cdf827a32a5f1e4e862b5cb
Joan Rasmussen <raz.joan@gmail.com>
2021-09-12 16:32:14 -0600

    Complete snack and vending-machine until it got SPICY

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,22 @@
 class Snack {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.itemsInStock = 0;
+  }
 
+  stockItems(num) {
+    this.itemsInStock += num;
+  }
+
+  removeItem() {
+    if(!this.itemsInStock) {
+      return `Sorry, no ${this.name} left in stock!`;
+    } else {
+      this.itemsInStock--;
+      return `Item taken! There are now ${this.itemsInStock} items left.`;
+    }
+  }
 }
 
 module.exports = Snack;
--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,18 @@
 class VendingMachine {
+  constructor(machine) {
+    this.id = machine.id;
+    this.isBroken = machine.isBroken;
+    this.snacks = [];
+  }
 
+  addSnacks(item) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name === item.name) {
+        return 'Sorry, that snack is already stocked! Try adding a different snack.';
+      }
+    }
+    this.snacks.push(item);
+  }
 }
 
 module.exports = VendingMachine;
