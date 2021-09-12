43ac6b2bf9680ffc08d5f747d6c1219cbc18bf4c
Joan Rasmussen <raz.joan@gmail.com>
2021-09-12 15:50:03 -0600

    Complete tests for Crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,23 @@
 class Craft {
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`
+  }
+
+  calculateProjectTotal() {
+    var total = 0;
+    for (var i = 0; i < this.materials.length; i++) {
+      var materialCost = this.materials[i].calculateMaterialCost();
+      total += materialCost;
+    }
+    return total;
+  }
 }
 
 module.exports = Craft;
