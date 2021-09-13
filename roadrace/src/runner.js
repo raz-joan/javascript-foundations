293b18d4c4cca2b451bd00472c19dae388eb5260
Joan Rasmussen <raz.joan@gmail.com>
2021-09-13 07:13:48 -0600

    Complete tests for RoadRace

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,25 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(number) {
+    this.milesRun += number;
+    this.fitness += number;
+  }
+
+  stretch() {
+    this.fitness += 0.5;
+  }
+
+  runRace(raceName, distance) {
+    this.completedRaces.push(raceName);
+    this.runSomeMiles(distance);
+  }
 }
 
 module.exports = Runner;
