a9970b0694d1297a9859138de2dd615cb1c8130b
Joan Rasmussen <raz.joan@gmail.com>
2021-09-14 07:17:55 -0600

    Complete snack and lunchbox tests for LunchBox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,30 @@
 class LunchBox {
+  constructor(lunchbox) {
+    this.owner = lunchbox.owner;
+    this.material = lunchbox.madeOf;
+    this.shape = lunchbox.shape;
+    this.color = lunchbox.color;
+    this.snacks = [];
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
+
+// search snacks array
+// check if element at each index is 'healthy'
+// if yes, add to new array
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,26 @@
 class Snack {
+  constructor(name)  {
+    this.deliciousLevel = "extra";
+    this.type = name;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
 
+  getEaten() {
+    this.amount -= 10;
+    if (this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
+
+  checkForHealthy() {
+    if (this.type.includes("Fruit") || this.type.includes("fruit")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Snack;
