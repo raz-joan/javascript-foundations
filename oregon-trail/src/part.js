1c56b112df2aef8cd5719c4b495cd1da1bb455ea
Joan Rasmussen <raz.joan@gmail.com>
2021-09-15 07:15:49 -0600

    Complete part-test for Oregon-Trail

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,22 @@
 class Part {
+  constructor(type) {
+    this.type = type;
+    this.broken = false;
+    this.numTimesBroken = 0;
+  }
 
+  break() {
+    this.numTimesBroken++;
+    this.broken = true;
+  }
+
+  repair() {
+    if (this.numTimesBroken >= 2) {
+      return `This ${this.type} has seen better days, we'll need a brand new one!`;
+    } else {
+      this.broken = false;
+    }
+  }
 }
 
 module.exports = Part;
