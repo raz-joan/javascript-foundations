b78a9ca46030bdd0470dab8a6eb6e93dd8ec47cb
Joan Rasmussen <raz.joan@gmail.com>
2021-09-15 07:08:33 -0600

    Complete gear-test for Oregon-Trail

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,17 @@
 class Gear {
-
+  constructor(item, number) {
+    this.type = item;
+    this.quantity = number;
+  }
+  checkForValidity() {
+    if (this.type !== 'ammunition' && this.type !== 'food' && this.type !=='clothes') {
+      var item = this.type;
+      this.type = null;
+      return `I don't think a ${item} will help us.`;
+    } else {
+      return `Great, we'll need lots of ${this.type}!`;
+    }
+  }
 }
 
 module.exports = Gear;
